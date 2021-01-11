e2815e1b789d2424ed164243702374e5a3153857
Kevin Harrington <mad.hephaestus@gmail.com>
2021-01-11 17:58:11 -0500

    taller tester

--- smallkatfeet.groovy
+++ smallkatfeet.groovy
@@ -6,23 +6,24 @@ import eu.mihosoft.vrl.v3d.Sphere
 
 def radius = 10
 def neckRad = 6
-def arclen=16.5
+def arclen=17.5
 def neckThicknes =2
 def theta = (arclen*360)/(2.0*3.14159*radius)
-def d = Math.sin((theta/2)/180.0*3.14159)*radius
+def internalAngle = (90-(theta/2))
+def d = Math.sin(Math.toRadians(internalAngle))*radius
 
-println d
+println d +" "+theta+" cir="+(3.14159*radius)+ " ind angle="+internalAngle
 
-CSG slicer = new Cylinder(radius, neckThicknes).toCSG()
+CSG slicer = new Cylinder(radius*2, neckThicknes).toCSG()
 				.difference(new Cylinder(neckRad,neckRad+neckThicknes, neckThicknes,15).toCSG())
 				.toZMax()
 				.movez(d)
 CSG slicer2 = new Cylinder(radius*2, radius*2).toCSG().toZMax()
-				.movez(d-neckThicknes)
+				.movez(d-neckThicknes-5)
 
 CSG ball  = new Sphere(radius,32, 16).toCSG()
 				.difference(slicer)
-				//.difference(slicer2)
-				.union(new Cylinder(radius-2, 10).toCSG().toZMax())
+				.difference(slicer2)
+				//.union(new Cylinder(radius-2, 10).toCSG().toZMax())
 				
 				
