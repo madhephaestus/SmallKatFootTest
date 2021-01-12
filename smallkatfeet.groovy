153932fd76836cc657488552a316d1d3f8d09a5e
Kevin Harrington <mad.hephaestus@gmail.com>
2021-01-11 19:23:03 -0500

    inset the nubin

--- smallkatfeet.groovy
+++ smallkatfeet.groovy
@@ -3,8 +3,9 @@ import eu.mihosoft.vrl.v3d.Cylinder
 import eu.mihosoft.vrl.v3d.Sphere
 
 //Your code here
-
-def radius = 10
+def capThickness=2.25
+def ballRadius = 10
+def radius = ballRadius-(capThickness/2.0)
 def neckRad = 6
 def arclen=18.5
 def neckThicknes =3.5
@@ -18,12 +19,15 @@ CSG slicer = new Cylinder(radius*2, neckThicknes).toCSG()
 				.difference(new Cylinder(neckRad,neckRad+neckThicknes, neckThicknes,15).toCSG())
 				.toZMax()
 				.movez(d)
-CSG slicer2 = new Cylinder(radius*2, radius*2).toCSG().toZMax()
-				.movez(d-neckThicknes-5)
+CSG slicer2 = new Cylinder(radius*2, radius*2).toCSG()
+				
+CSG foot = new Sphere(ballRadius,32, 16).toCSG()
+			.difference(slicer2.movez(d-neckThicknes))
 
 CSG ball  = new Sphere(radius,32, 16).toCSG()
 				.difference(slicer)
-				.difference(slicer2)
-				//.union(new Cylinder(radius-2, 10).toCSG().toZMax())
+				//.difference(slicer2)
+				.union(foot)
+				.union(new Cylinder(radius-2, 10).toCSG().toZMax())
 				
 				
